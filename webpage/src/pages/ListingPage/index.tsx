1ca314822600fdfdebdacb1cda480a4b7e960894
Josiel Borges <josielborges555@gmail.com>
2023-03-23 14:39:54 -0300

    feat: listing page contacts

--- webpage/src/pages/ListingPage/index.tsx
+++ webpage/src/pages/ListingPage/index.tsx
@@ -16,8 +16,13 @@ import {
   Box,
   AccordionPanel,
   AccordionIcon,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
 } from "@chakra-ui/react";
-import { StyledList } from "./style";
+import { ListingPageStyled, StyledList } from "./style";
 
 interface IClient {
   id: string;
@@ -34,11 +39,12 @@ interface IContact {
   email: string;
   phone: string;
   createdAt: Date;
-  client: IClient | null;
+  client: IClient;
 }
 
 export default () => {
   const [clientsInfo, setClientsInfo] = useState<IClient[]>([]);
+  const [contactsInfo, setContactsInfo] = useState<IContact[]>([]);
 
   useEffect(() => {
     const loadClients = async () => {
@@ -50,51 +56,100 @@ export default () => {
       }
     };
 
+    const loadContacts = async () => {
+      try {
+        const { data } = await api.get("/contacts");
+        setContactsInfo(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
     loadClients();
+    loadContacts();
   }, []);
 
   return (
-    <>
-      <StyledList>
-        {clientsInfo.map((client, id) => {
-          return (
-            <li>
-              <Card direction={{ base: "column", sm: "row" }}>
-                <Stack>
-                  <CardHeader>
-                    <Heading size="md">{client.name}</Heading>
-                    <Text py="2">Email: {client.email}</Text>
-                    <Text py="2">Phone: {client.phone}</Text>
-                  </CardHeader>
-                  <CardBody>
-                    <Heading size="sm">Client's contacts</Heading>
-                    <Accordion allowToggle>
-                      {client.contacts.map((contact, id) => (
-                        <>
-                          <AccordionItem>
-                            <h2>
-                              <AccordionButton>
-                                <Box as="span" flex="1" textAlign="left">
-                                  {contact.name}
-                                </Box>
-                                <AccordionIcon />
-                              </AccordionButton>
-                            </h2>
-                            <AccordionPanel pb={4}>
-                              <Text>Email: {contact.email}</Text>
-                              <Text>Phone: {contact.phone}</Text>
-                            </AccordionPanel>
-                          </AccordionItem>
-                        </>
-                      ))}
-                    </Accordion>
-                  </CardBody>
-                </Stack>
-              </Card>
-            </li>
-          );
-        })}
-      </StyledList>
-    </>
+    <ListingPageStyled>
+      <Tabs variant="unstyled">
+        <TabList className="tabNavBar">
+          <div>
+            <Tab _selected={{ color: "white", bg: "blue.500" }}>Clients</Tab>
+            <Tab _selected={{ color: "white", bg: "green.400" }}>Contacts</Tab>
+          </div>
+          <Button></Button>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <StyledList>
+              {clientsInfo.map((client, id) => {
+                return (
+                  <li>
+                    <Card direction={{ base: "column", sm: "row" }}>
+                      <Stack>
+                        <CardHeader>
+                          <Heading size="md">{client.name}</Heading>
+                          <Text py="2">Email: {client.email}</Text>
+                          <Text py="2">Phone: {client.phone}</Text>
+                        </CardHeader>
+                        <CardBody>
+                          <Heading size="sm">Contacts</Heading>
+                          <Accordion allowToggle>
+                            {client.contacts.map((contact, id) => (
+                              <>
+                                <AccordionItem>
+                                  <h2>
+                                    <AccordionButton>
+                                      <Box as="span" flex="1" textAlign="left">
+                                        {contact.name}
+                                      </Box>
+                                      <AccordionIcon />
+                                    </AccordionButton>
+                                  </h2>
+                                  <AccordionPanel pb={4}>
+                                    <Text>Email: {contact.email}</Text>
+                                    <Text>Phone: {contact.phone}</Text>
+                                  </AccordionPanel>
+                                </AccordionItem>
+                              </>
+                            ))}
+                          </Accordion>
+                        </CardBody>
+                      </Stack>
+                    </Card>
+                  </li>
+                );
+              })}
+            </StyledList>
+          </TabPanel>
+
+          <TabPanel>
+            <StyledList>
+              {contactsInfo.map((contact, id) => {
+                return (
+                  <li>
+                    <Card direction={{ base: "column", sm: "row" }}>
+                      <Stack>
+                        <CardHeader>
+                          <Heading size="md">{contact.name}</Heading>
+                          <Text py="2">Email: {contact.email}</Text>
+                          <Text py="2">Phone: {contact.phone}</Text>
+                        </CardHeader>
+                        <CardBody>
+                          <Heading size="sm">Client</Heading>
+                          <Text>Name: {contact.client.name}</Text>
+                          <Text>Email: {contact.client.email}</Text>
+                          <Text>Phone: {contact.client.phone}</Text>
+                        </CardBody>
+                      </Stack>
+                    </Card>
+                  </li>
+                );
+              })}
+            </StyledList>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </ListingPageStyled>
   );
 };
--- webpage/src/pages/ListingPage/style.ts
+++ webpage/src/pages/ListingPage/style.ts
@@ -1,10 +1,29 @@
 import styled from "styled-components";
 
+export const ListingPageStyled = styled.div`
+  .tabNavBar {
+    position: sticky;
+    top: 0;
+    z-index: 101;
+    background-color: white;
+    width: 100%;
+    height: 50px;
+    justify-content: space-evenly;
+    align-items: center;
+    > div {
+      display: flex;
+    }
+    > button {
+      background-color: aquamarine;
+    }
+  }
+`;
+
 export const StyledList = styled.ul`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
-  padding: 30px;
+  padding: 20px;
   gap: 10px;
   > li {
     max-height: 20vh;
