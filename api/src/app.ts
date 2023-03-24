dc046d3a44f8012d9f7c9f8c1f9d1066e13ec4ea
Josiel Borges <josielborges555@gmail.com>
2023-03-24 13:32:40 -0300

    feat: add cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,7 @@
 import "express-async-errors";
 import "reflect-metadata";
 import express from "express";
+import cors from "cors";
 
 import errorHandler from "./errors";
 import clientsRoutes from "./routers/clients.routes";
@@ -9,6 +10,8 @@ import contactsRoutes from "./routers/contacts.routes";
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/clients", clientsRoutes);
 app.use("/contacts", contactsRoutes);
